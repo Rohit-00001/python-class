955e4903b7a6425667843ce34197774400a8ed8f
Rohit-00001 <rohit@apie.tech>
2025-07-03 18:49:03 +0530

    day 6

---
+++ 6 day/index.js
@@ -0,0 +1,37 @@
+// console.log("Hello, js");
+// alert("Hello, js");
+
+let a = 10;
+
+let z = a;
+z = 200;
+
+console.log("a" + a);
+console.log("z" + z);
+
+let b = 10.22;
+console.log("b" + b);
+
+console.log(typeof a);
+console.log(typeof b);
+
+let d = "Rohit";
+console.log(typeof d);
+
+let e = false;
+console.log(typeof e);
+
+// {},[],()
+
+// let a = {
+//   name: "Rohit",
+//   age: 22,
+// };
+
+let arr1 = [1, 2, 3, 4, 4];
+
+let arr2 = arr1;
+arr2[2] = "hello";
+console.log(typeof arr2);
+
+function abc() {}
